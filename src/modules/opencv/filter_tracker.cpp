b9419385eacbda2693ba7177e7c8c43a86579726
Jean-Baptiste Mardelle <jb@kdenlive.org>
2016-04-16 18:48:01 +0200

    Follow MLT convention to store anaylsis data in "results" and cleanup. Allow selecting tracker algorithm and drawing ellipse and arrows as well as rectangle

--- src/modules/opencv/filter_tracker.cpp
+++ src/modules/opencv/filter_tracker.cpp
@@ -18,57 +18,47 @@
  */
 
 #include <framework/mlt.h>
-#include <stdlib.h> // calloc(), free()
+#include <stdlib.h>
 #include <opencv2/tracking.hpp>
 
 
-struct cv_data
+typedef struct
 {
         cv::Ptr<cv::Tracker> tracker;
         cv::Rect2d boundingBox;
         bool initialized;
-};
+} private_data;
 
-typedef struct cv_data *tracker_data;
 
-/** Get the image.
-*/
-static int filter_get_image( mlt_frame frame, uint8_t **image, mlt_image_format *format, int *width, int *height, int writable )
+static void apply( mlt_filter filter, private_data* data, int width, int height, int position, int length )
 {
-	int error = 0;
-	mlt_filter filter = (mlt_filter)mlt_frame_pop_service( frame );
+	mlt_properties properties = MLT_FILTER_PROPERTIES( filter );
+        mlt_rect rect = mlt_properties_anim_get_rect(properties, "results", position, length);
+        data->boundingBox.x = std::max(rect.x, 1.0);
+        data->boundingBox.y= std::max(rect.y, 1.0);
+        data->boundingBox.width = rect.w;
+        data->boundingBox.height = rect.h;
+}
 
+
+static void analyse( mlt_filter filter, cv::Mat cvFrame, private_data* data, int width, int height, int position, int length )
+{
 	mlt_properties filter_properties = MLT_FILTER_PROPERTIES( filter );
-        mlt_position position = mlt_filter_get_position( filter, frame );
-	mlt_position length = mlt_filter_get_length2( filter, frame );
 
-        mlt_service_lock( MLT_FILTER_SERVICE(filter) );
-        tracker_data data = (tracker_data) mlt_properties_get_data( filter_properties, "tracker_data", NULL );
-
-        // Clip already analysed, don't re-process
-        if( mlt_properties_get_int( filter_properties, "analyse" ) == 0 && mlt_properties_get(filter_properties, "motion_tracking")) {
-                mlt_rect rect = mlt_properties_anim_get_rect(filter_properties, "motion_tracking", position, length);
-                if( mlt_properties_get_int( filter_properties, "show_rect" ) != 0 ) {
-                        // Get the current image
-                        *format = mlt_image_rgb24;
-                        error = mlt_frame_get_image( frame, image, format, width, height, 1 );
-                        cv::Mat cvFrame = cv::Mat(*height, *width, CV_8UC3, *image);
-                        mlt_color rect_color = mlt_properties_get_color( filter_properties, "rect_color" );
-                        data->boundingBox.x = std::max(rect.x, 1.0);
-                        data->boundingBox.y= std::max(rect.y, 1.0);
-                        data->boundingBox.width = rect.w;
-                        data->boundingBox.height = rect.h;
-                        cv::rectangle( cvFrame, data->boundingBox, cv::Scalar( rect_color.r, rect_color.g, rect_color.b ), mlt_properties_get_int( filter_properties, "show_rect" ), 1);
-                } else {
-                        // do nothing
-                        error = mlt_frame_get_image( frame, image, format, width, height, 1 );
+        // Create tracker and initialize it
+        if (!data->initialized) {
+                // Build tracker
+                if( data->tracker == NULL ) {
+                        const char *algo = mlt_properties_get( filter_properties, "algo" );
+                        if ( algo == NULL || strcmp(algo, "" ) ) {
+                                data->tracker = cv::Tracker::create("KCF");
+                        } else {
+                                data->tracker = cv::Tracker::create(algo);
+                        }
                 }
-        } else {
-                // Get the current image
-                *format = mlt_image_rgb24;
-                error = mlt_frame_get_image( frame, image, format, width, height, 1 );
-                cv::Mat cvFrame = cv::Mat(*height, *width, CV_8UC3, *image);
-                if (!data->initialized) {
+                if( data->tracker == NULL ) {
+                        fprintf(stderr, "Tracker initialized FAILED\n");
+                } else {
                         if (data->boundingBox.width <1 || data->boundingBox.height <1) {
                                 mlt_rect rect = mlt_properties_get_rect(filter_properties, "rect");
                                 data->boundingBox.x = std::max(rect.x, 1.0);
@@ -85,29 +75,93 @@ static int filter_get_image( mlt_frame frame, uint8_t **image, mlt_image_format 
                         if (data->tracker->init( cvFrame, data->boundingBox )) {
                                 data->initialized = true;
                         }
-                } else {
-                        if (data->tracker->update( cvFrame, data->boundingBox ) ) {
-                        }
                 }
-                if( mlt_properties_get_int( filter_properties, "show_rect" ) != 0 ) {
-                        mlt_color rect_color = mlt_properties_get_color( filter_properties, "rect_color" );
-                        cv::rectangle( cvFrame, data->boundingBox, cv::Scalar( rect_color.r, rect_color.g, rect_color.b ), mlt_properties_get_int( filter_properties, "show_rect" ), 1);
-                }
+        } else {
+                data->tracker->update( cvFrame, data->boundingBox );
         }
 
-        if( mlt_properties_get_int( filter_properties, "analyse" ) == 1 ) {
-                mlt_rect rect;
-                rect.x = data->boundingBox.x;
-                rect.y = data->boundingBox.y;
-                rect.w = data->boundingBox.width;
-                rect.h = data->boundingBox.height;
-                mlt_properties_anim_set_rect(filter_properties, "motion_tracking", rect, position, length, mlt_keyframe_linear);
-        }
+        // Store results in temp variable
+        mlt_rect rect;
+        rect.x = data->boundingBox.x;
+        rect.y = data->boundingBox.y;
+        rect.w = data->boundingBox.width;
+        rect.h = data->boundingBox.height;
+        rect.o = 0;
+        mlt_properties_anim_set_rect(filter_properties, "_results", rect, position, length, mlt_keyframe_linear);
 
-        mlt_service_unlock( MLT_FILTER_SERVICE(filter) );
+        if ( position + 1 == length )
+        {
+                //Analysis finished, store results
+                mlt_properties_set(filter_properties, "results", mlt_properties_get(filter_properties, "_results" ) );
+                // Discard temporary data
+                mlt_properties_set(filter_properties, "_results", (char*) NULL );
+        }
+}
+
+
+/** Get the image.
+*/
+static int filter_get_image( mlt_frame frame, uint8_t **image, mlt_image_format *format, int *width, int *height, int writable )
+{
+	int error = 0;
+	mlt_filter filter = (mlt_filter)mlt_frame_pop_service( frame );
+        mlt_properties filter_properties = MLT_FILTER_PROPERTIES( filter );
+        mlt_position position = mlt_filter_get_position( filter, frame );
+	mlt_position length = mlt_filter_get_length2( filter, frame );
+
+        mlt_service_lock( MLT_FILTER_SERVICE(filter) );
+        int shape_width = mlt_properties_get_int( filter_properties, "shape_width" );
+        cv::Mat cvFrame;
+        if (shape_width == 0) {
+                error = mlt_frame_get_image( frame, image, format, width, height, 1 );
+        }
+        else
+        {
+                *format = mlt_image_rgb24;
+                error = mlt_frame_get_image( frame, image, format, width, height, 1 );
+                cvFrame = cv::Mat(*height, *width, CV_8UC3, *image);
+        }
+        private_data* data = (private_data*)filter->child;
+
+        char* results = mlt_properties_get( filter_properties, "results" );
+	if( results && strcmp( results, "" ) )
+	{
+                // Clip already analysed, don't re-process
+                apply( filter, data, *width, *height, position, length );
+	}
+	else
+	{
+		analyse( filter, cvFrame, data, *width, *height, position, length );
+	}
 	
+	// Paint overlay shape
+	if (shape_width != 0) {
+                // Get the OpenCV image
+                mlt_color shape_color = mlt_properties_get_color( filter_properties, "shape_color" );
+                switch ( mlt_properties_get_int( filter_properties, "shape" ) ) {
+                case 2:
+                        // Arrow
+                        cv::arrowedLine( cvFrame, cv::Point(data->boundingBox.x + data->boundingBox.width/2, data->boundingBox.y - data->boundingBox.height/2), cv::Point(data->boundingBox.x + data->boundingBox.width/2, data->boundingBox.y), cv::Scalar( shape_color.r, shape_color.g, shape_color.b ), std::max( shape_width, 1 ), 4, 0, .2);
+                        break;
+                case 1:
+                        // Ellipse
+                        {
+                                cv::RotatedRect bounding = cv::RotatedRect(cv::Point2f(data->boundingBox.x + data->boundingBox.width/2, data->boundingBox.y + data->boundingBox.height/2), cv::Size2f(data->boundingBox.width, data->boundingBox.height), 0);
+                                cv::ellipse( cvFrame, bounding, cv::Scalar( shape_color.r, shape_color.g, shape_color.b ), shape_width, 1);
+                        }
+                        break;
+                case 0:
+                default:
+                        // Rectangle
+                        cv::rectangle( cvFrame, data->boundingBox, cv::Scalar( shape_color.r, shape_color.g, shape_color.b ), shape_width, 1);
+                        break;
+        }
+        }
+
+        mlt_service_unlock( MLT_FILTER_SERVICE( filter ) );
 	return error;
 }
+
 
 /** Filter processing.
 */
@@ -120,6 +174,10 @@ static mlt_frame filter_process( mlt_filter filter, mlt_frame frame )
 
 static void filter_close( mlt_filter filter )
 {
+        private_data* data = (private_data*)filter->child;
+        free ( delete data->tracker );
+        free ( delete data );
+        filter->child = NULL;
 	filter->close = NULL;
 	filter->parent.close = NULL;
 	mlt_service_close( &filter->parent );
@@ -133,25 +191,17 @@ extern "C" {
 mlt_filter filter_tracker_init( mlt_profile profile, mlt_service_type type, const char *id, char *arg )
 {
 	mlt_filter filter = mlt_filter_new();
-        tracker_data data = (tracker_data)calloc( 1, sizeof(struct cv_data) ); 
+        private_data* data = (private_data*)calloc( 1, sizeof(private_data) );
 
 	if ( filter && data)
 	{
-		mlt_properties properties = MLT_FILTER_PROPERTIES( filter );
-
-                // Init tracker
-                data->tracker = cv::Tracker::create(cv::String("MIL"));
-                if( data->tracker == NULL ) {
-                    fprintf(stderr, "Tracker initialized FAILED\n");
-                } else {
-                    fprintf(stderr, "Tracker initialized\n");
-                }
+                mlt_properties_set_int( MLT_FILTER_PROPERTIES( filter ), "shape_width", 1 );
                 data->initialized = false;
                 data->boundingBox.x = 0;
                 data->boundingBox.y= 0;
                 data->boundingBox.width = 0;
                 data->boundingBox.height = 0;
-                mlt_properties_set_data( properties, "tracker_data", data, 0, NULL, NULL );
+                filter->child = data;
 
                 // Create a unique ID for storing data on the frame
 		filter->close = filter_close;
